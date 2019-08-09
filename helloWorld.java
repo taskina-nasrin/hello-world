d5df147ed3e9152153579d6ede7ac80daecaf6c7
taskina-nasrin <53804332+taskina-nasrin@users.noreply.github.com>
2019-08-09 15:39:56 +0600

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,8 @@ public class helloWorld{
   public static void main(String[] args){
   
   System.out.println("Hello World!");
-  
+  String s1 = "Hello"
+  String s2 = "World"
+    System.out.println(s1 + s2 );
   }
   }
