e43b8c5d81335b95303d3060e1e882731fa7d3e9
Taskina Nasrin <53804332+taskina-nasrin@users.noreply.github.com>
2019-08-10 15:11:25 +0600

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,8 @@ public class helloWorld{
   
   System.out.println("Hello World!");
   String s1 = "Hello"
-  String s2 = "World"
+  String s2 = "I am Taskina"
     System.out.println(s1 + s2 );
+    System.out.println("I am new to the git hub want to learn everything step by step");
   }
   }
