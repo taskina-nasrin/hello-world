f21dfaec3afe7f4ac67004cea484bacbd35adedf
taskina-nasrin <taskinanasrin@gmail.com>
2019-08-15 11:01:28 +0600

    added

---
+++ hi.java
@@ -0,0 +1,9 @@
+class Student{
+       int roll;
+       String name;
+   }
+   class S{
+     public static void public static void main(String[] args) {
+       System.out.println("Hello taskina")
+     }
+   }
---
+++ hi2.java
@@ -0,0 +1,7 @@
+public class EX1 {
+    public static void main(String[] args) {
+        String custname="Merry Smith",itemdesc="shirt";
+        String message;
+
+        message=custname+"wants to buy a" + itemdesc;
+        System.out.println (message);
