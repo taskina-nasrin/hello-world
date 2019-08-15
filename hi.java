0132b5fdd0182e02ce57e454575811be16aad6d7
taskina-nasrin <taskinanasrin@gmail.com>
2019-08-15 11:04:39 +0600

    object

--- hi.java
+++ hi.java
@@ -4,6 +4,9 @@ class Student{
    }
    class S{
      public static void public static void main(String[] args) {
-       System.out.println("Hello taskina")
-     }
+       System.out.println("Hello taskina");
+       Student s1 = new Student();
+       s1.roll=8;
+       System.out.println("Roll Number:" s1);
+          }
    }
